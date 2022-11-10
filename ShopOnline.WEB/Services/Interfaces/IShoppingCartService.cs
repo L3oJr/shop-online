38e0983762b809ad40502e25950dc9c2e804b1d9
L3oJr <leojuniortj@gmail.com>
2022-11-10 20:16:44 -0300

    Cart page and logic

---
+++ ShopOnline.WEB/Services/Interfaces/IShoppingCartService.cs
@@ -0,0 +1,10 @@
+﻿using ShopOnline.Models.Dtos;
+
+namespace ShopOnline.Web.Services.Interfaces
+{
+    public interface IShoppingCartService
+    {
+        Task<IEnumerable<CartItemDto>> GetItems(int userId);
+        Task<CartItemDto> AddItem(CartItemToAddDto cartItemToAddDto);
+    }
+}
